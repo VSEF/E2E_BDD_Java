781a844ca2966a7445ba1c2a382251b6d26c08c5
VSEF <sv.grishau@gmail.com>
2018-02-05 09:44:15 -0800

    Added new annotation

--- src/test/java/running/RunCukesTests.java
+++ src/test/java/running/RunCukesTests.java
@@ -1,8 +1,10 @@
 package running;
 
+import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
+@CucumberOptions(format = {"json:target/cucumber.json"})
 public class RunCukesTests {
 }
