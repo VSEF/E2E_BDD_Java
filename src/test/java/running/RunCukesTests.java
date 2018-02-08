8c290c8e4d43b6c23a899f4d1e64643a2ccc0c04
VSEF <sv.grishau@gmail.com>
2018-02-08 09:57:40 -0800

    tiny changes

--- src/test/java/running/RunCukesTests.java
+++ src/test/java/running/RunCukesTests.java
@@ -5,6 +5,6 @@ import cucumber.api.junit.Cucumber;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features= "features", glue = "steps", format = {"json:target/cucumber.json"})
+@CucumberOptions(features= "features", glue = "steps", plugin =  {"pretty","html:target/html", "json:cucumber.json"})
 public class RunCukesTests {
 }
